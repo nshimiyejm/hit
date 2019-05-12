f6c0f22e3e83495f2ffc8e97a8cf8eca4e0d83c4
Johnny E <nshimiyejm@gmail.com>
2019-05-11 22:36:06 -0400

    First app
    Add a paper app in the project that will be used to parse different types of documents and text

--- paper_researh/researh/urls.py
+++ paper_researh/researh/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import papers.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', papers.views.paperparse, name='home')
 ]
